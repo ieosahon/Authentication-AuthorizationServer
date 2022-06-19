938e53c3bce710122c1401244213bf336d7aa9e2
Osahon Ighodaro <osahonie@gmail.com>
2022-06-19 08:20:03 +0100

    created DbContext

---
+++ JobsApi/Data/JobsDbContext.cs
@@ -0,0 +1,15 @@
+﻿using JobsAppApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobsApi.Data
+{
+    public class JobsDbContext : DbContext
+    {
+        public JobsDbContext(DbContextOptions<JobsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Job> Jobs { get; set; }
+    }
+}
