58f67927312224f47b1ade32b3f8e2f1116e3ecc
Osahon Ighodaro <osahonie@gmail.com>
2022-06-19 02:38:49 +0100

    modified the contoller class

---
+++ JobsApi/Controllers/JobsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobsApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JobsController : ControllerBase
+    {
+
+        private readonly ILogger<JobsController> _logger;
+
+        public JobsController(ILogger<JobsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Job> Get()
+        {
+            
+        }
+    }
+}
