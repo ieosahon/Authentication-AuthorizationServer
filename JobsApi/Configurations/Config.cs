94c4bb81db326d296234230cf689d8611ac1feb0
Osahon Ighodaro <osahonie@gmail.com>
2022-06-19 09:42:36 +0100

    Done with db context registration

---
+++ JobsApi/Configurations/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace JobsApi.Configurations
+{
+    public class Config : IConfig
+    {
+        public bool RunDbMigrations { get; set; }
+        public bool SeedDb { get; set; }
+    }
+}
--- JobsApi/Startup.cs
+++ JobsApi/Startup.cs
@@ -1,7 +1,10 @@
+using JobsApi.Configurations;
+using JobsApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,8 +28,39 @@ namespace JobsApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Registration of Config class
+            services.AddSingleton<IConfig>(Configuration.GetSection("CustomConfig")?.Get<Config>());
 
             services.AddControllers();
+
+            //Registration of AddDbContexts method
+            AddDbContexts(services);
+        }
+
+        public void AddDbContexts(IServiceCollection services)
+        {
+            var debugLogging = new Action<DbContextOptionsBuilder>(opt =>
+            {
+#if DEBUG
+                // this will log EF generated SQL commands to the console
+                opt.UseLoggerFactory(LoggerFactory.Create(builder =>
+                {
+                    builder.AddConsole();
+                }));
+
+                // This will log the params for those commands to the console
+                opt.EnableSensitiveDataLogging();
+                opt.EnableDetailedErrors();
+#endif
+            });
+            
+            // Actual DbContext registration
+            services.AddDbContext<JobsDbContext>(options =>
+            {
+                var conString = Configuration.GetConnectionString("sqldb-job")?? "name= JobDb";
+                options.UseSqlServer(conString, opt => opt.EnableRetryOnFailure(5));
+                debugLogging(options);
+            }, ServiceLifetime.Transient);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
