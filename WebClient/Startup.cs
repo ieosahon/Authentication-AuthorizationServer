ab59cd7f022e54acd5b038a93fd8a4d565db3083
Osahon Ighodaro <osahonie@gmail.com>
2022-06-30 07:59:21 +0100

    registered services in startup class

--- WebClient/Startup.cs
+++ WebClient/Startup.cs
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebClient.Http;
+using WebClient.JobServices;
 
 namespace WebClient
 {
@@ -23,6 +25,10 @@ namespace WebClient
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IJobService, JobService>();
+
+            services.AddTransient<IHttpClient, GeneralHttpClient>();
+            
             services.AddControllersWithViews();
         }
 
